31af3fd695292625c599aec47cd9cbcd387cf6c3
Emilly Lamotte <79487290+emillylamotte@users.noreply.github.com>
2021-03-30 13:52:59 -0300

    Update jogoMentalista.js

--- jogoMentalista/jogoMentalista.js
+++ jogoMentalista/jogoMentalista.js
@@ -3,10 +3,10 @@ console.log(numeroSecreto)
 
 var tentativas = 3
 
-var acerto = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/temporary/d64be7100a065bb4afb4ca19e349f341.png"+">"+'<div class="imagem">') 
-var eMaior = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/temporary/179682ebf643efacec29dbd26369c084.png"+">"+'<div class="imagem">') 
-var eMenor = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/temporary/05dd47fb986b21145002180cee1f021c.png"+">"+'<div class="imagem">') 
-var fim = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/temporary/f281fd87c7bbb7cd7096b29702626876.png"+">"+'<div class="imagem">') 
+var acerto = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/permalink/210330/1da339661c4a02ea2e87f0ca89ad4e2b.png"+">"+'<div class="imagem">') 
+var eMaior = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/permalink/210330/8bb5ce0e20c87249719f390eb5e2443e.png"+">"+'<div class="imagem">') 
+var eMenor = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/permalink/210330/701901acf4a7069f32451a69de05f0f9.png"+">"+'<div class="imagem">') 
+var fim = ('<div class="imagem">'+"<img src="+ "https://fontmeme.com/permalink/210330/34c6e201067e44615e39e0c88c4dc43f.png"+">"+'<div class="imagem">') 
 
 function adicionarChute(){
 
@@ -16,7 +16,7 @@ function adicionarChute(){
    if (numeroSecreto == chute){
        var acertou = document.getElementById("imagem")  
        acertou.innerHTML = acerto
-     }
+      }
     if (numeroSecreto < chute){
          var menor = document.getElementById("imagem")  
          menor.innerHTML = eMenor
@@ -32,3 +32,4 @@ function adicionarChute(){
         fimDeJogo.innerHTML = fim
     }
 } 
+
